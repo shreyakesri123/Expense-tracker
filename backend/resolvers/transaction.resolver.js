1793bab83e6c0975effb5c6a3b8e39c1cc097056
shreya <shreyakesri41755@gmail>
2024-07-09 20:47:59 +0530

    transaction resolver added

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -2,32 +2,33 @@ import Transaction from "../models/transaction.model.js";
 import User from "../models/user.model.js";
 
 const transactionResolver={
-    Query:{
-        transactions: async (_, __, context) => {
-			try {
-				if (!context.getUser()) throw new Error("Unauthorized");
-				const userId = await context.getUser()._id;
-
-				const transactions = await Transaction.find({ userId });
-				return transactions;
-			} catch (err) {
-				console.error("Error getting transactions:", err);
-				throw new Error("Error getting transactions");
-			}
-		},
-        transaction: async (_, { transactionId }) => {
-			try {
-				const transaction = await Transaction.findById(transactionId);
-				return transaction;
-			} catch (err) {
-				console.error("Error getting transaction:", err);
-				throw new Error("Error getting transaction");
-			}
-		},
+    
+		Query: {
+			transactions: async (_, __, context) => {
+				try {
+					if (!context.getUser()) throw new Error("Unauthorized");
+					const userId = await context.getUser()._id;
+	
+					const transactions = await Transaction.find({ userId });
+					return transactions;
+				} catch (err) {
+					console.error("Error getting transactions:", err);
+					throw new Error("Error getting transactions");
+				}
+			},
+			transaction: async (_, { transactionId }) => {
+				try {
+					const transaction = await Transaction.findById(transactionId);
+					return transaction;
+				} catch (err) {
+					console.error("Error getting transaction:", err);
+					throw new Error("Error getting transaction");
+				}
+			},
         //TODO => ADD CATEGORY STATISTICS 
     },
     Mutation:{
-        createTransaction: async (_, { input }, context) => {
+		createTransaction: async (_, { input }, context) => {
 			try {
 				const newTransaction = new Transaction({
 					...input,
@@ -40,7 +41,7 @@ const transactionResolver={
 				throw new Error("Error creating transaction");
 			}
 		},
-        updateTransaction: async (_, { input }) => {
+		updateTransaction: async (_, { input }) => {
 			try {
 				const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {
 					new: true,
